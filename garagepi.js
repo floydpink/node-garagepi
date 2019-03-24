b056c3a5e1d1716dc69989c502ec83b1a1e311d1
Floyd Pink <imbleedingme@googlemail.com>
2019-03-24 19:50:42 -0400

    upgrade dependencies

--- garagepi.js
+++ garagepi.js
@@ -1,12 +1,12 @@
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var GPIO = require("onoff").Gpio;
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-var startTakingSnaps = false;
+const path = require('path');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const GPIO = require("onoff").Gpio;
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+let startTakingSnaps = false;
 
 require('console-stamp')(console, '[HH:MM:ss]');
 
@@ -23,7 +23,7 @@ app.get('/', function(req, res) {
   res.render('index.html');
 });
 
-var state = 'closed';
+let state = 'closed';
 app.get('/api/clickbutton', function(req, res) {
   state = state == 'closed' ? 'open' : 'closed';
 
@@ -41,18 +41,18 @@ app.get('/api/status', function(req, res) {
 });
 
 function outputSequence(pin, seq, timeout) {
-  var gpio = new GPIO(4, 'out');
+  const gpio = new GPIO(4, 'out');
   gpioWrite(gpio, pin, seq, timeout);
 }
 
 function gpioWrite(gpio, pin, seq, timeout) {
-  if (!seq || seq.length <= 0) { 
+  if (!seq || seq.length <= 0) {
     console.log('closing pin:', pin);
     gpio.unexport();
     return;
   }
 
-  var value = seq.substr(0, 1);
+  const value = seq.substr(0, 1);
   seq = seq.substr(1);
   setTimeout(function() {
     console.log('gpioWrite, value:', value, ' seq:', seq);
@@ -63,16 +63,16 @@ function gpioWrite(gpio, pin, seq, timeout) {
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 function takeSnaps() {
-  var autoSnapshot = setTimeout(function() {
-    var imgPath = path.join(__dirname, 'public/images');
-    var cmd = 'raspistill -w 640 -h 480 -q 80 -o ' + imgPath + '/garage.jpg';
-    var exec = require('child_process').exec;
+  return setTimeout(function () {
+    const imgPath = path.join(__dirname, 'public/images');
+    const cmd = 'raspistill -w 640 -h 480 -q 80 -o ' + imgPath + '/garage.jpg';
+    const exec = require('child_process').exec;
     exec(cmd, function (error, stdout, stderr) {
       if (error !== null) {
         console.log('exec error: ' + error);
@@ -80,13 +80,11 @@ function takeSnaps() {
       }
       io.emit('snapshot', 'ready');
       console.log('snapshot created...');
-      if(startTakingSnaps) {
+      if (startTakingSnaps) {
         takeSnaps();
       }
     });
   }, 0);
-
-  return autoSnapshot;
 }
 
 io.on('connection', function(socket){
@@ -100,7 +98,7 @@ io.on('connection', function(socket){
   });
 });
 
-var port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000;
 server.listen(port, function() {
   console.log('GaragePi listening on port:', port);
 });
